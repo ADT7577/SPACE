2fb6854e31dcc73807d24fffc65158213c592b96
Alexsa <adt7577@nyu.edu>
2022-05-03 02:23:30 -0400

    commit

--- dreams.js
+++ dreams.js
@@ -2,16 +2,19 @@ import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threej
 import { OrbitControls } from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/examples/jsm/controls/OrbitControls.js';
 // import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
-let camera3D, scene, renderer, cube;
-let dir = 0.01;
+let camera3D, scene, renderer, sphere;
+let dir = 0.5;
 let controls;
+
+
+let img;
 
 
 function init3D() {
 
     //Scene
     scene = new THREE.Scene();
-    camera3D = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera3D = new THREE.PerspectiveCamera(125, window.innerWidth / window.innerHeight, 0.1, 1000);
   
 
 
@@ -20,44 +23,46 @@ function init3D() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    
+
     // The Object
-    const geometry = new THREE.BoxGeometry();
+    // const radius = 3;  
+    // const segments = 10; 
+    const geometry = new THREE.SphereGeometry(2.5, 32, 16);
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    sphere = new THREE.Mesh(geometry, material);
     
+    scene.add(sphere);
+    
+    //lights
+    const light = new THREE.AmbientLight( 0xffffff, 0.5 );
+    scene.add( ambient );
+
 
     //Controls
     controls = new OrbitControls(camera3D, renderer.domElement);
     camera3D.position.z = 5;
 
-    //BACKGROUND
-    let bgGeometery = new THREE.SphereGeometry(1000, 40, 40);
+    // //BACKGROUND
+    let bgGeometery = new THREE.SphereGeometry(1000, 60, 60);
     bgGeometery.scale(-1, 1, 1);
-    let panotexture = new THREE.TextureLoader().load("Future2.jpg");
+    let panotexture = new THREE.TextureLoader().load("Future8.jpg");
     let backMaterial = new THREE.MeshBasicMaterial({ map: panotexture });
     let back = new THREE.Mesh(bgGeometery, backMaterial);
     scene.add(back);
-
-
         animate();
     }
    
 
-
 function animate() {
+
     requestAnimationFrame(animate);
     controls.update();
-   /* cube.scale.x += dir;
-    cube.scale.y += dir;
-    cube.scale.z += dir;
-    cube.rotation.y += 0.01;
-    cube.rotation.x += 0.01;
-    if (cube.scale.x > 4 || cube.scale.x < -4) {
-        dir = -dir;
+    sphere.rotation.y += 0.01;
+    sphere.rotation.x += 0.01;
+    if (sphere.scale.x > 1 || sphere.scale.x < 1) {
+        dir * 0.01
     }
-    */
+ 
     renderer.render(scene, camera3D);
 }
 
